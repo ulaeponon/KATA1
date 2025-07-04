7a9907606febaa4aa676b738c1b6f41748961d86
Ursula Eponon <ulaeponon@gmail.com>
2025-07-04 12:30:42 +0200

    add final work

--- kata1.js
+++ kata1.js
@@ -8,17 +8,28 @@ form.addEventListener('submit', function (event) {
   // console.log(author);
   form.reset();
 });
+let counter=0;
 function addQuote(quotes, authors) {
   const quoteText = document.createElement("p");
   quoteText.className = "text";
   quoteText.textContent = quotes;
+  
   const quoteAuthor = document.createElement("p");
   quoteAuthor.className = "author";
   quoteAuthor.textContent = authors;
+  
   const quoteDiv = document.createElement("div");
   quoteDiv.className = "quote";
   quoteDiv.appendChild(quoteText);
   quoteDiv.appendChild(quoteAuthor);
+  
   const quoteList = document.getElementById("quote-list");
   quoteList.appendChild(quoteDiv);
+  
+  counter ++;
+  updateCitationCount()
 };
+function updateCitationCount() {
+  const title = document.getElementById("citation-count");
+  title.textContent = counter + (counter <= 1 ? " citation" : " citations");
+}
