789f092db2ab406b6b6a3c8b5743aa21782d4738
Ursula Eponon <ulaeponon@gmail.com>
2025-07-04 11:32:52 +0200

    add new update

--- kata1.js
+++ kata1.js
@@ -8,3 +8,17 @@ form.addEventListener('submit', function (event) {
   // console.log(author);
   form.reset();
 });
+function addQuote(quotes, authors) {
+  const quoteText = document.createElement("p");
+  quoteText.className = "text";
+  quoteText.textContent = quotes;
+  const quoteAuthor = document.createElement("p");
+  quoteAuthor.className = "author";
+  quoteAuthor.textContent = authors;
+  const quoteDiv = document.createElement("div");
+  quoteDiv.className = "quote";
+  quoteDiv.appendChild(quoteText);
+  quoteDiv.appendChild(quoteAuthor);
+  const quoteList = document.getElementById("quote-list");
+  quoteList.appendChild(quoteDiv);
+};
